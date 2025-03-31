f341869fb974ae9f6957b635c34598e3445df8a4
Dev Eduardo <eduardoaugustolimabueno@outlook.com.br>
2025-03-31 20:57:21 -0300

    Resolvendo problemas de extensão de arquivos

--- src/routes/ping/handler.ts
+++ src/routes/ping/handler.ts
@@ -1,6 +1,6 @@
 import { FastifyTypedInstace } from "#types/FastifyTypedInstace.js";
 import { z } from "zod";
-import { prisma } from "#libs/prisma";
+import { prisma } from "#libs/prisma.js";
 
 export default function (server: FastifyTypedInstace) {
     server.get("/",
